f0feff1992cd705d34860f4620a5cd49621f2ef1
Julian Flesch <julianflesch@gmail.com>
2019-08-26 16:28:39 +0200

    reference to data. Vignette is work in progress

---
+++ R/zzz.r
@@ -0,0 +1,16 @@
+#@importFrom scater computeSumFactors normalize quickCluster
+#@importFrom SingleCellExperiment SinglecellExperiment assay
+
+# Documentation of the RData objects
+#' empcdf.rf
+#'
+#' An empirical cumulative distribution function of a simulated Brownian bridge
+#' distribution. It is used an empirical quantile function to determine 
+#' p-values in the asymptotic wasserstein test function wasserstein.test.asy
+#' 
+#' @name empcdf.ref  
+#' @docType data
+#' @keywords data
+#' @export
+empcdf.ref <- NULL
+
